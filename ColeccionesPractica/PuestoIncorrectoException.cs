dc98e3153a8af071d77283d03fc8569bcd67740f
paulLozano90 <plc90@hotmail.es>
2015-10-27 12:51:07 +0100

    ColeccionesPractica

---
+++ ColeccionesPractica/PuestoIncorrectoException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColeccionesPractica
+{
+    public class PuestoIncorrectoException : Exception
+    {
+        public PuestoIncorrectoException(String msg) : base(msg)
+        {
+
+        }
+    }
+}

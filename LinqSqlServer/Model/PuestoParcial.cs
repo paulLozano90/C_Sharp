580d32c62fba3666efdce65ebda1ff8f394afc19
paulLozano90 <plc90@hotmail.es>
2015-10-27 14:27:02 +0100

    LinqSqlServer

---
+++ LinqSqlServer/Model/PuestoParcial.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqSqlServer.Model
+{
+    public partial class Cargo
+    {
+        public override string ToString()
+        {
+            return $"{nombre} {salarioBase}";
+        }
+    }
+}
---
+++ LinqSqlServer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LinqSqlServer.Model;
+
+namespace LinqSqlServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ctx = new EmpleadosEntities();
+
+            //INSERT:
+            var p = new Cargo()
+            {
+                nombre = "Tecnico IT",
+                salarioBase = 3000
+            };
+            //ctx.Cargo.Add(p);
+            //ctx.SaveChanges();
+
+            //MOSTRAR LISTA
+            foreach (var puesto in ctx.Cargo)
+            {
+                Console.WriteLine(puesto);
+            }
+            Console.ReadLine();
+        }
+    }
+}
